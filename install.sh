226fd71fd3ead7e269bcebb80da87bbb0368213b
motoki nagaoka <motoki.23ar@gmail.com>
2016-11-02 17:44:14 +0900

    Add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+check_file() {
+    if [ -e "$1" ]; then
+        echo "Error, $1 already exists."
+        echo "Please rename or remove $1."
+        exit
+    fi
+}
+check_file "$HOME/.tmux.conf"
+check_file "$HOME/.tmux/tmux_conf"
+git clone https://github.com/motokii/tmux_conf.git $HOME/.tmux/tmux_conf
+ln -s $HOME/.tmux/tmux_conf/tmux.conf $HOME/.tmux
