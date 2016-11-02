000e0b66cbdc7101bc0dcd73e8ce562507236d8b
motoki nagaoka <motoki.23ar@gmail.com>
2016-11-02 18:07:04 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -8,5 +8,8 @@ check_file() {
 }
 check_file "$HOME/.tmux.conf"
 check_file "$HOME/.tmux/tmux_conf"
+
+echo ""
+echo "Installing..."
 git clone https://github.com/motokii/tmux_conf.git $HOME/.tmux/tmux.conf
-ln -s $HOME/.tmux/tmux_conf/tmux.conf $HOME/.tmux
+ln -s $HOME/.tmux/tmux_conf/tmux.conf $HOME/.tmux.conf
